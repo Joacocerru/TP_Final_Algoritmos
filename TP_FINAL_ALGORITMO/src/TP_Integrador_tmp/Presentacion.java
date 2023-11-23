1778b789527a47d5bbb2f45303dc547ba14606aa
Chelo78 <valeirasmd@gmail.com>
2023-11-23 17:40:00 -0300

    ,

--- TP_FINAL_ALGORITMO/src/TP_Integrador_tmp/Presentacion.java
+++ TP_FINAL_ALGORITMO/src/TP_Integrador_tmp/Presentacion.java
@@ -16,9 +16,9 @@ public class Presentacion {
     
     // USER STORY 1: CARGAR DATOS DESDE ARCHIVO .CSV   ////////////////////////////
      
-    //DataFrame df = new DataFrame("C:\\Users\\Valentín\\OneDrive\\ESTUDIOS\\UNSAM\\ALGORITMOS\\TP_FINAL_ALGORITMO\\TP_FINAL_ALGORITMO\\prueba1.csv", ",", "S");
+    DataFrame dfx = new DataFrame("C:\\Documentos\\n67745\\Unsam\\Algoritmos 1\\Java\\TP_Final\\TP_Final_Algoritmos\\TP_FINAL_ALGORITMO\\prueba1.csv", ",", "N");
     DataFrame df =  new DataFrame("C:\\Documentos\\n67745\\Unsam\\Algoritmos 1\\Java\\TP_Final\\TP_Final_Algoritmos\\TP_FINAL_ALGORITMO\\prueba8.csv", ",", "S");
-    DataFrame df1 =  new DataFrame("C:\\Documentos\\n67745\\Unsam\\Algoritmos 1\\Java\\TP_Final\\TP_Final_Algoritmos\\TP_FINAL_ALGORITMO\\prueba8.csv", ",", "S");
+    DataFrame df1 = new DataFrame("C:\\Documentos\\n67745\\Unsam\\Algoritmos 1\\Java\\TP_Final\\TP_Final_Algoritmos\\TP_FINAL_ALGORITMO\\prueba8.csv", ",", "S");
     //DataFrame df = new DataFrame("C:\\Documentos\\n67745\\Unsam\\Algoritmos 1\\Java\\TP_Final\\TP_Final_Algoritmos\\TP_FINAL_ALGORITMO\\prueba1.csv", ",", "S");
     //DataFrame df =  new DataFrame("C:\\Users\\Hernan\\Desktop\\TP_FINAL_ALGORITMO\\prueba1.csv", ",", "S");
     
